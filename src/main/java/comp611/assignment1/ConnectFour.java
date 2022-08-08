1a88ab9b6840273a22df731c6b48a1bb39fc28b4
Nathan <nathand1278@gmail.com>
2022-08-08 17:44:55 +1200

    Initial commit

---
+++ src/main/java/comp611/assignment1/ConnectFour.java
@@ -0,0 +1,9 @@
+package comp611.assignment1;
+
+public class ConnectFour {
+
+    public static void main(String[] args) {
+        // startup the server
+        System.out.println("Welcome to Connect 4!");
+    }
+}
